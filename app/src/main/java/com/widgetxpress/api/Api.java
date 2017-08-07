27d5e9e02345399347e3ab6b99cae31a7238cdf7
Irving <irving.gerza@gmail.com>
2017-08-07 09:56:08 -0500

    Crear petición para agregar actividad

--- app/src/main/java/com/widgetxpress/api/Api.java
+++ app/src/main/java/com/widgetxpress/api/Api.java
@@ -1,9 +1,12 @@
 package com.widgetxpress.api;
 
+import com.widgetxpress.model.Actividad;
 import com.widgetxpress.model.ResponseActividad;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
@@ -15,5 +18,8 @@ public interface Api
 {
     @GET("getActivities.php?=")
     Call<ResponseActividad>getActividades(@Query("id") String id);
+
+    @POST("addActivity.php?actividad=")
+    Call<Actividad> agregarActividad(@Body  Actividad actividad);
 
 }
