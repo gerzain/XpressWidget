911b522a3d3f0ffe07407eb341f9882408eb4620
Irving <irving.gerza@gmail.com>
2017-08-07 09:20:30 -0500

    Corregir la consulta de actividades con id de google invalido

--- app/src/main/java/com/widgetxpress/WidgetFetchService.java
+++ app/src/main/java/com/widgetxpress/WidgetFetchService.java
@@ -1,13 +1,23 @@
 package com.widgetxpress;
 
+import android.app.PendingIntent;
 import android.app.Service;
 import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Looper;
+import android.preference.PreferenceManager;
 import android.support.annotation.AttrRes;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.widget.RemoteViews;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -18,6 +28,7 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.widgetxpress.activity.Login;
 import com.widgetxpress.api.Api;
 import com.widgetxpress.model.Actividad;
 import com.widgetxpress.model.ResponseActividad;
@@ -30,15 +41,21 @@ import retrofit2.Callback;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import static com.widgetxpress.Xpress.ACTION_GOTO_LOGIN;
+
 /**
  * Created by Irving on 18/07/2017.
  */
 
-public class WidgetFetchService extends Service {
+public class WidgetFetchService extends Service
+{
 
     private int appWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
     private static final String TAG = WidgetFetchService.class.getSimpleName();
     public static List<Actividad> actividades = new ArrayList<>();
+    private boolean isLogin;
+    private String id_google;
+
 
     @Nullable
     @Override
@@ -47,65 +64,97 @@ public class WidgetFetchService extends Service {
     }
 
     @Override
+    public void onDestroy() {
+       Log.e(TAG,"Se llama onDestroy");
+        super.onDestroy();
+    }
+
+    @Override
+    public void onCreate() {
+       Log.e(TAG,"Se llama onCreate");
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        id_google=sharedPref.getString("pref_id",null);
+        //Log.e(TAG,id_google);
+        super.onCreate();
+    }
+
+    @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         if (intent.hasExtra(AppWidgetManager.EXTRA_APPWIDGET_ID))
             appWidgetId = intent.getIntExtra(
                     AppWidgetManager.EXTRA_APPWIDGET_ID,
                     AppWidgetManager.INVALID_APPWIDGET_ID);
-        new ObtenerDatos().execute();
+        fetchData();
         return super.onStartCommand(intent, flags, startId);
     }
 
-    public class ObtenerDatos extends AsyncTask<Void, Void, Void> {
+    public void fetchData() {
 
-        @Override
-        protected Void doInBackground(Void... voids) {
+        if (Login.id_google == null)
+        {
+            AppWidgetManager appWidgetManager =AppWidgetManager.getInstance(getApplicationContext());
+            ComponentName watchWidget=new ComponentName(getApplicationContext(),Xpress.class);
+            RemoteViews views = new RemoteViews(getApplicationContext().getPackageName(), R.layout.xpress_widget);
 
-           fetchData();
-            return null;
+            views.setEmptyView(R.id.widget_main_view,R.id.empty_view);
+            Intent google_account=new Intent(getApplicationContext(),Login.class);
+            google_account.setAction(Xpress.ACTION_GOTO_LOGIN);
+            google_account.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+            google_account.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,appWidgetId);
+            views.setOnClickPendingIntent(R.id.empty_view,PendingIntent.getActivity(getApplicationContext(),0,google_account,PendingIntent.FLAG_UPDATE_CURRENT));
+            appWidgetManager.updateAppWidget(watchWidget,views);
+            stopSelf();
+
+        } else
+
+        {
+
+
+            isLogin = true;
+            String url = "http://xpress.genniux.net/php/xpress2.0/agenda/";
+            Log.e(TAG, "fetchData");
+
+            Gson gson = new GsonBuilder()
+                    .setDateFormat("yyyy-MM-dd HH:mm:ss")
+                    .create();
+
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(url)
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .build();
+
+            Api servicio = retrofit.create(Api.class);
+            Call<ResponseActividad> responseActividadCall = servicio.getActividades(Login.id_google);
+
+            responseActividadCall.enqueue(new Callback<ResponseActividad>() {
+                @Override
+                public void onResponse(Call<ResponseActividad> call, retrofit2.Response<ResponseActividad> response) {
+                    if (response.isSuccessful())
+
+                    {
+                        String status = response.body().getStatus();
+                        Log.i(TAG, status);
+                        ResponseActividad responseActividad = response.body();
+
+
+                        actividades = responseActividad.getActividades();
+
+                        populateWidget();
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<ResponseActividad> call, Throwable t) {
+                    Log.e(TAG, " onFailure: " + t.getMessage());
+                }
+            });
         }
     }
 
-    public void fetchData()
-    {
-
-        String url = "http://xpress.genniux.net/php/xpress2.0/agenda/";
-
-        Gson gson=new GsonBuilder()
-                .setDateFormat("yyyy-MM-dd HH:mm:ss")
-                .create();
-
-        Retrofit retrofit=new Retrofit.Builder()
-                .baseUrl(url)
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .build();
-
-        Api servicio=retrofit.create(Api.class);
-        Call<ResponseActividad> responseActividadCall=servicio.getActividades("105536481769353298421");
-
-        responseActividadCall.enqueue(new Callback<ResponseActividad>() {
-            @Override
-            public void onResponse(Call<ResponseActividad> call, retrofit2.Response<ResponseActividad> response) {
-                if(response.isSuccessful())
-                {
-                    String status=response.body().getStatus();
-                    Log.i(TAG,status);
-                    ResponseActividad responseActividad=response.body();
-
-                    actividades=responseActividad.getActividades();
-
-                    populateWidget();
-                }
-            }
-
-            @Override
-            public void onFailure(Call<ResponseActividad> call, Throwable t) {
-                Log.e(TAG, " onFailure: " + t.getMessage());
-            }
-        });
 
 
-    }
+
+
     private void populateWidget() {
         Intent widgetUpdateIntent = new Intent();
         widgetUpdateIntent.setAction(Xpress.DATA_FETCHED);
@@ -114,6 +163,18 @@ public class WidgetFetchService extends Service {
         sendBroadcast(widgetUpdateIntent);
 
         this.stopSelf();
+    }
+    public static void sendRefreshBroadcast(Context context) {
+    Intent intent = new Intent(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+    intent.setComponent(new ComponentName(context, Xpress.class));
+    context.sendBroadcast(intent);
+}
+
+    public boolean isOnline() {
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        return (networkInfo != null && networkInfo.isConnected());
     }
 
 }
