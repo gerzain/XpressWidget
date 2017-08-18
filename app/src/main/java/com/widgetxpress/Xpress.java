95f30885bb5af5172d3f0a2ea4a295488576514f
Irving <irving.gerza@gmail.com>
2017-08-18 16:29:41 -0500

    Agregar la opcion de actulizar

--- app/src/main/java/com/widgetxpress/Xpress.java
+++ app/src/main/java/com/widgetxpress/Xpress.java
@@ -7,6 +7,8 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.support.v4.app.TaskStackBuilder;
 import android.util.Log;
 import android.widget.RemoteViews;
@@ -26,9 +28,13 @@ public class Xpress extends AppWidgetProvider
     public static final String DATA_FETCHED="com.widgetxpress.DATA_FETCHED";
     public static final String TAG=Xpress.class.getSimpleName();
     public static final String ACTION_GOTO_NEW_TASK = "ActionGotoNewTask";
-    public static  final String ACTION_GOTO_LOGIN="ActionLogin";
+    public static final String ACTION_GOTO_LOGIN="ActionLogin";
     public static final String ACTION_GOTO_ACCOUNT="ActionAccount";
+    public static final String ACTION_GOTO_LOGOUT="com.widgetxpress.ACTION_GOTO_LOGOUT";
+    public static final String ACTION_REFRESH="com.widgetxpress.ACTION_REFRESH";
     public static final String EXTRA_ID="ID";
+    private static Handler sWorkerQueue;
+    private static HandlerThread sWorkerThread;
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
@@ -58,26 +64,50 @@ public class Xpress extends AppWidgetProvider
     public void onReceive(Context context, Intent intent) {
         super.onReceive(context, intent);
         Log.i(TAG, "onReceive" + intent.getAction());
-      Toast.makeText(context, "onReceive: " + intent.getAction(), Toast.LENGTH_SHORT).show();
+      //Toast.makeText(context, "onReceive: " + intent.getAction(), Toast.LENGTH_SHORT).show();
+       final  int appWidgetId = intent.getIntExtra(
+                AppWidgetManager.EXTRA_APPWIDGET_ID,
+                AppWidgetManager.INVALID_APPWIDGET_ID);
         if(intent.getAction().equalsIgnoreCase(DATA_FETCHED))
         {
-            int appWidgetId = intent.getIntExtra(
-                    AppWidgetManager.EXTRA_APPWIDGET_ID,
-                    AppWidgetManager.INVALID_APPWIDGET_ID);
+
             AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
             RemoteViews remoteViews=updateWidgetListView(context,appWidgetId);
             appWidgetManager.updateAppWidget(appWidgetId,remoteViews);
         }else if(intent.getAction().equalsIgnoreCase(AppWidgetManager.ACTION_APPWIDGET_UPDATE))
         {
-            int appWidgetId = intent.getIntExtra(
-                    AppWidgetManager.EXTRA_APPWIDGET_ID,
-                    AppWidgetManager.INVALID_APPWIDGET_ID);
+
             AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
             RemoteViews remoteViews=updateWidgetListView(context,appWidgetId);
             appWidgetManager.updateAppWidget(appWidgetId,remoteViews);
+        }else if( intent.getAction().equalsIgnoreCase(ACTION_REFRESH))
+        {
+
+            Log.i(TAG,"ActionRefresh");
+            updateWidget(context);
+
+
         }
     }
 
+    /**
+     * Permite notificar cuando ocurre un cambio dentro de la lista
+     * @param context El conexto de clase Context
+     */
+
+    private void updateWidget(Context context) {
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+        int appWidgetIds[] = appWidgetManager.getAppWidgetIds(new ComponentName(context, Xpress.class));
+        appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.widget_main_view);
+    }
+
+
+    /***
+     * El metodo para obtener los datos en la lista y establecer acciones mediante los botones
+     * @param context El contexto de clase Context
+     * @param appWidgetId el id del widget
+     * @return  las vistas de
+     */
     private RemoteViews updateWidgetListView(Context context, int appWidgetId)
     {
         RemoteViews remoteViews=new RemoteViews(context.getPackageName(),R.layout.xpress_widget);
@@ -85,19 +115,19 @@ public class Xpress extends AppWidgetProvider
         Intent svcIntent = new Intent(context, WidgetService.class);
         svcIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,appWidgetId);
         svcIntent.setData(Uri.parse(svcIntent.toUri(Intent.URI_INTENT_SCHEME)));
-        /*Intent login=new Intent(context, Login.class);
-        login.setAction(ACTION_GOTO_LOGIN);
-        login.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,appWidgetId);
-        login.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);*/
         Intent google_account=new Intent(context,Login.class);
         google_account.setAction(ACTION_GOTO_ACCOUNT);
         google_account.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
         Intent agregar = new Intent(context, AgregarActividad.class);
         agregar.setAction(ACTION_GOTO_NEW_TASK);
         agregar.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-        remoteViews.setOnClickPendingIntent(R.id.widget_title_add, PendingIntent.getActivity(context,0,agregar,PendingIntent.FLAG_UPDATE_CURRENT));
+        remoteViews.setOnClickPendingIntent(R.id.widget_title_add,
+                PendingIntent.getActivity(context,0,agregar,
+                PendingIntent.FLAG_UPDATE_CURRENT));
         //remoteViews.setOnClickPendingIntent(R.id.btn_login,PendingIntent.getActivity(context,0,login,PendingIntent.FLAG_UPDATE_CURRENT));
-        remoteViews.setOnClickPendingIntent(R.id.widget_title_star,PendingIntent.getActivity(context,0,google_account,PendingIntent.FLAG_UPDATE_CURRENT));
+        remoteViews.setOnClickPendingIntent(R.id.widget_title_star,PendingIntent.
+                getActivity(context,0,google_account,
+                        PendingIntent.FLAG_UPDATE_CURRENT));
         /***
          * Para acceder a un elemento de la lista
          */
@@ -109,15 +139,30 @@ public class Xpress extends AppWidgetProvider
         remoteViews.setPendingIntentTemplate(R.id.widget_main_view, clickPendingIntentTemplate);
 
         remoteViews.setRemoteAdapter(R.id.widget_main_view,svcIntent);
-        remoteViews.setEmptyView(R.id.widget_main_view,R.layout.widget_card_empty);
+        remoteViews.setEmptyView(R.id.widget_main_view,R.id.empty_view);
+
+
+        /**
+         * Para acceder al boton de refrescar
+         */
+
+        Intent refresh=new Intent(context,Xpress.class);
+        refresh.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,appWidgetId);
+        refresh.setAction(Xpress.ACTION_REFRESH);
+        PendingIntent refreshPendingIntent=PendingIntent.getBroadcast(context,appWidgetId,refresh,PendingIntent.FLAG_UPDATE_CURRENT);
+        remoteViews.setOnClickPendingIntent(R.id.refresh,refreshPendingIntent);
 
         return  remoteViews;
 
     }
 
+
+
+
     @Override
     public void onEnabled(Context context) {
         // Enter relevant functionality for when the first widget is created
+
     }
 
     @Override
