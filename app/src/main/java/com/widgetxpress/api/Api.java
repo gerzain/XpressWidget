1ab1fd8c16eae061d7ec090158befe2cf8b52a22
Irving <irving.gerza@gmail.com>
2017-09-01 17:49:08 -0500

    Agregar doc sobre el funcionamiento

--- app/src/main/java/com/widgetxpress/api/Api.java
+++ app/src/main/java/com/widgetxpress/api/Api.java
@@ -1,10 +1,14 @@
 package com.widgetxpress.api;
 
 import com.widgetxpress.model.Actividad;
+import com.widgetxpress.model.Actividades;
+import com.widgetxpress.model.EditarActividad;
 import com.widgetxpress.model.ResponseActividad;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
@@ -16,6 +20,11 @@ import retrofit2.http.Query;
 
 public interface Api
 {
+    /**
+     * Función que permite obtener las actividades
+     * @param id El id unico de Google
+     * @return Las actividades que tiene el usuario
+     */
     @GET("getActivities.php?=")
     Call<ResponseActividad>getActividades(@Query("id") String id);
 
